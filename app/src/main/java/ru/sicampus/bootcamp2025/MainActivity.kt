135aac70dc7ded5a5cb198ea2570ff84cd864d37
Ксении <ksushavarvarkina@gmail.com>
2025-01-31 17:45:26 +0300

    Добавлена разметка основных фрагментов

--- app/src/main/java/ru/sicampus/bootcamp2025/MainActivity.kt
+++ app/src/main/java/ru/sicampus/bootcamp2025/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.sicampus.bootcamp2025
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,11 +8,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.profile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.profile)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
